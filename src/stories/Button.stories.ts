75289337adcd1a7e993d4f11bbf06f32dd4980fd
Benjamin Kindle <Benjamin.kindle@hyland.com>
2021-10-01 13:27:45 -0400

    produce issue with moduleMetadata when generic type is used in Meta

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,6 +1,12 @@
 // also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
-import { Story, Meta } from "@storybook/angular/types-6-0";
+import { CommonModule } from "@angular/common";
+import { moduleMetadata } from "@storybook/angular";
+import { Story, Meta } from "@storybook/angular";
 import Button from "./button.component";
+
+interface StoryArgs {
+  disabled: boolean;
+}
 
 export default {
   title: "Example/Button",
@@ -8,7 +14,12 @@ export default {
   argTypes: {
     backgroundColor: { control: "color" },
   },
-} as Meta;
+  decorators: [
+    moduleMetadata({
+      imports: [CommonModule],
+    }),
+  ],
+} as Meta<Partial<StoryArgs>>;
 
 const Template: Story<Button> = (args: Button) => ({
   props: args,
