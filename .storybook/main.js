c35d53e33269d1e471f4531ea090017a6494f8df
Benjamin Kindle <Benjamin.kindle@hyland.com>
2021-10-01 12:57:22 -0400

    reproduce angular storystorev7 issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/angular",
   ],
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  core: {
+    builder: "webpack5",
+  },
+  features: {
+    storyStoreV7: true,
+  },
+};
